5dce0741751037cd659ae415c19f1b864db8aaa1
Stefan Butz <stefan260699@gmail.com>
2021-10-15 09:55:37 +0200

    Spring boot hello world

---
+++ demo/src/test/java/de/othr/sw/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.othr.sw.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
