5d5575a1c95d394352b264e36e4e9af218af617b
Stefan Butz <stefan260699@gmail.com>
2021-11-09 10:58:42 +0100

    Generate Spring Project

---
+++ yetra/src/main/java/de/othr/sw/yetra/YetraApplication.java
@@ -0,0 +1,13 @@
+package de.othr.sw.yetra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YetraApplication.class, args);
+    }
+
+}
---
+++ yetra/src/test/java/de/othr/sw/yetra/YetraApplicationTests.java
@@ -0,0 +1,13 @@
+package de.othr.sw.yetra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
