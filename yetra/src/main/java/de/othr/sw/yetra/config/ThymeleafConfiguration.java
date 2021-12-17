62f483faf915f34b022affaa761c373464d79886
Stefan Butz <stefan260699@gmail.com>
2021-12-17 11:13:24 +0100

    Add thymeleaf layout dialect

---
+++ yetra/src/main/java/de/othr/sw/yetra/config/ThymeleafConfiguration.java
@@ -0,0 +1,13 @@
+package de.othr.sw.yetra.config;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ThymeleafConfiguration {
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+}
