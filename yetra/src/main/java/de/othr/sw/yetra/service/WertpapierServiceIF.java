ff8b8de297e7eea40239170c6d521ced114d366d
Stefan Butz <stefan260699@gmail.com>
2022-01-02 22:12:17 +0100

    add todo

--- yetra/src/main/java/de/othr/sw/yetra/service/WertpapierServiceIF.java
+++ yetra/src/main/java/de/othr/sw/yetra/service/WertpapierServiceIF.java
@@ -9,8 +9,10 @@ import de.othr.sw.yetra.entity.Wertpapier;
 import java.util.Collection;
 
 public interface WertpapierServiceIF {
+    //TODO: use Iterable
     Collection<Wertpapier> wertpapiereAbfragen();
 
+    //TODO: use Iterable twice
     Collection<Wertpapier> wertpapiereAbfragen(Collection<Wertpapier> wertpapiere) throws NichtGefundenException;
 
     WertpapierDetails wertpapierDetailsAbfragen(Wertpapier wertpapier, Zeitspanne zeitspanne) throws NichtGefundenException;
