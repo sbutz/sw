a61caec73971864518b66aa1ed5d4f8623841ad1
Stefan Butz <stefan260699@gmail.com>
2022-01-17 12:31:06 +0100

    bot: create random orders

--- yetra/src/main/java/de/othr/sw/yetra/setup/OrderBot.java
+++ yetra/src/main/java/de/othr/sw/yetra/setup/OrderBot.java
@@ -1,11 +1,15 @@
 package de.othr.sw.yetra.setup;
 
+import com.google.common.collect.Iterables;
 import de.othr.sw.yetra.entity.Order;
 import de.othr.sw.yetra.entity.OrderType;
+import de.othr.sw.yetra.entity.Share;
 import de.othr.sw.yetra.entity.User;
 import de.othr.sw.yetra.repository.OrderRepository;
+import de.othr.sw.yetra.repository.ShareRepository;
 import de.othr.sw.yetra.repository.UserRepository;
 import de.othr.sw.yetra.service.OrderServiceIF;
+import de.othr.sw.yetra.util.MathUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Scope;
@@ -13,6 +17,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+
+import java.util.Random;
 
 import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_SINGLETON;
 
@@ -22,29 +28,34 @@ import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_SING
 @Scope(SCOPE_SINGLETON)
 public class OrderBot {
 
-    private static final int interval = 15;
+    private static final int interval = 20;
 
-    private static final int delay = 5;
+    private static final int delay = 10;
 
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository userRepo;
 
     @Autowired
     private OrderServiceIF orderService;
-    
+
     @Autowired
-    private OrderRepository orderRepository;
+    private OrderRepository orderRepo;
+
+    @Autowired
+    private ShareRepository shareRepo;
 
     private User bot;
 
+    private Random random = new Random();
+
     @PostConstruct
     public void init() {
-        bot = userRepository.findUserByUsername("bot").get();
+        bot = userRepo.findUserByUsername("bot").get();
     }
 
-    @Scheduled(fixedDelay = interval*1000, initialDelay = delay*1000)
+    @Scheduled(fixedDelay = interval * 1000, initialDelay = delay * 1000)
     public void completeOpenOrders() {
-        for (Order order : orderRepository.findOpenOrders()) {
+        for (Order order : orderRepo.findOpenOrders()) {
             Order o = new Order();
             o.setType(order.getType() == OrderType.BUY ? OrderType.SELL : OrderType.BUY);
             o.setShare(order.getShare());
@@ -53,5 +64,22 @@ public class OrderBot {
             o.setClient(bot);
             orderService.createOrder(o);
         }
+    }
+
+    @Scheduled(fixedDelay = interval * 1000, initialDelay = delay * 1000)
+    public void randomOrders() {
+        Iterable<Share> shares = shareRepo.findAll();
+        Share s = Iterables.get(shares, random.nextInt(Iterables.size(shares)));
+        double price = s.getCurrentPrice()
+                + MathUtils.round(s.getCurrentPrice() * MathUtils.random(-0.02, 0.02), 3);
+
+
+        Order order = new Order();
+        order.setType(OrderType.SELL);
+        order.setShare(s);
+        order.setQuantity(1);
+        order.setUnitPrice(price);
+        order.setClient(bot);
+        orderService.createOrder(order);
     }
 }
