8b03d5034ad0d984cc6d672b32eaf4613c6d468a
Stefan Butz <stefan260699@gmail.com>
2022-01-17 08:42:46 +0100

    application name as constant

--- yetra/src/main/java/de/othr/sw/yetra/config/LoggingConfiguration.java
+++ yetra/src/main/java/de/othr/sw/yetra/config/LoggingConfiguration.java
@@ -2,6 +2,7 @@ package de.othr.sw.yetra.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -11,9 +12,12 @@ import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_SING
 @Configuration
 public class LoggingConfiguration {
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     @Bean
     @Scope(SCOPE_SINGLETON)
     public Logger getLogger() {
-        return LoggerFactory.getLogger("yetra");
+        return LoggerFactory.getLogger(appName);
     }
 }
