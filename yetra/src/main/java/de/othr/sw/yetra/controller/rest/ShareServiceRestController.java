ea598a4ba9e8e5c0ebaf87882ff1d1f5013a43b7
Stefan Butz <stefan260699@gmail.com>
2022-01-18 00:42:01 +0100

    fix rest parameter

--- yetra/src/main/java/de/othr/sw/yetra/controller/rest/ShareServiceRestController.java
+++ yetra/src/main/java/de/othr/sw/yetra/controller/rest/ShareServiceRestController.java
@@ -2,7 +2,6 @@ package de.othr.sw.yetra.controller.rest;
 
 import de.othr.sw.yetra.dto.*;
 import de.othr.sw.yetra.entity.Share;
-import de.othr.sw.yetra.service.ServiceException;
 import de.othr.sw.yetra.service.ShareServiceIF;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -10,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_SINGLETON;
@@ -25,11 +25,11 @@ public class ShareServiceRestController {
     @GetMapping("")
     @PreAuthorize("hasAuthority('SHARES_READ')")
     public Iterable<Share> getShares(
-            @RequestParam(name= "filter", required = false) Optional<Iterable<String>> filter,
+            @RequestParam(name= "filter", required = false) Optional<String[]> filter,
             @RequestParam(name= "name", required = false) Optional<String> name
     ) {
         if (filter.isPresent())
-            return shareService.getShares(filter.get());
+            return shareService.getShares(Arrays.asList(filter.get()));
         if (name.isPresent())
             return shareService.getSharesNameContains(name.get());
         else
